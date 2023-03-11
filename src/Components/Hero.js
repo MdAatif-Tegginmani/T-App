9d6381cb784f9dd685d60f5c9acd07644fd2ca5f
Md Aatif Tegginmani <mdaatif84@gmail.com>
2023-03-12 01:12:50 +0530

    Updated t-app

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,8 +5,8 @@ function Hero() {
   return (
     <>
 
-    <div className="relative isolate overflow-hidden bg-gray-600 py-24 sm:py-32">
-    <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"/>
+    <div className="relative isolate overflow-hidden bg-gray-600 py-20 sm:py-32">
+    <img src="https://static.thehoneycombers.com/wp-content/uploads/sites/2/2022/07/The-Clan-Hotel.png" alt="" className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"/>
    
   
     <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -27,7 +27,7 @@ function Hero() {
        <Cards cardHeading="Top Choices" cardText="Total 800+ destination
        you can work with" ></Cards>
        <Cards cardHeading='Quality Guidance' cardText="Our tour Guide Has 25 Years Experience"></Cards>
-       <Cards cardHeading='Easy Booking' cardText='Best is Class Ticket Booking System'></Cards>  
+       <Cards cardHeading='Easy Booking' cardText='Best is Class Ticket Booking'></Cards>  
        
        </div>
        
